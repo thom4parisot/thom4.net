e5cc987806d01ccc3b862b9a1408a2a702cbfdc7
Thomas Parisot <thomas@oncle-tom.net>
2013-07-19 12:45:47 +0100

    Adding disqus in main.js

--- themes/oncletom/source/assets/js/main.js
+++ themes/oncletom/source/assets/js/main.js
@@ -1,4 +1,16 @@
-(function(document, undefined){
+(function(document, context, undefined){
+  var tag = document.querySelector("link[rel='canonical']");
+  
+  context.disqus_shortname = tag.getAttribute("data-disqus-shortname");
+  context.disqus_url = tag.getAttribute("href");
+
+  if (document.getElementById("disqus_thread")){ 
+    var dsq = document.createElement('script');
+    dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('body')[0]).appendChild(dsq);
+  }
+  
   var onScroll = (function(tag){
     if (!document.querySelector){
       return function(){};
@@ -23,4 +35,4 @@
     document.addEventListener('scroll', onScroll);
     document.querySelector('#services [data-toggle="collapse"]').addEventListener('click', toggleCollapse);
   }
-})(document);
+})(document, window);
