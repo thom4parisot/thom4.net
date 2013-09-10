7295a8e6e211a9cba5a04a1fc02ccd483e33ddb0
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-09-10 17:30:03 +0100

    Fixed navigation issues due to i18n module BC changes

--- scripts/tag-repost.js
+++ scripts/tag-repost.js
@@ -1,25 +1,29 @@
 "use strict";
 
-var i18n = new hexo.i18n();
-i18n.load("languages/tags", function(){});
+var translate = require('hexo/lib/i18n');
+var i18n = new translate();
 
-hexo.extend.tag.register('repost', function(args, content){
-  var data = {
-    "url": args[0] || '',
-    "title": args[1] || '',
-    "domain": args[0].match(/\/\/([^\/]+)/)[1].replace('www.', '')
-  };
+i18n.load('languages/', function(){
+  hexo.extend.tag.register('repost', function(args, content){
+    var __ = i18n.get('fr');
+    var data = {
+      "url": args[0] || '',
+      "title": args[1] || '',
+      "domain": args[0].match(/\/\/([^\/]+)/)[1].replace('www.', '')
+    };
 
-  function replacer(occurence){
-    return occurence.replace(/%([^%]+)%/g, function(m, key){
-      return (data[key] && data[key].replace(/(^"|"$)/g, "")) || '';
-    });
-  }
+    function replacer(occurence){
+      return occurence.replace(/%([^%]+)%/g, function(m, key){
+        return (data[key] && data[key].replace(/(^"|"$)/g, "")) || '';
+      });
+    }
 
-  var text = i18n.get.apply(this, ["repost_tag"].concat([
-    '<a href="http://%domain%" class="domain-link" rel="external">%domain%</a>',
-    '<a href="%url%" class="source-link" rel="external">%title%</a>'
-  ].map(replacer)));
+    var text = __.apply(this, ["repost_tag"].concat([
+      '<a href="http://%domain%" class="domain-link" rel="external">%domain%</a>',
+      '<a href="%url%" class="source-link" rel="external">%title%</a>'
+    ].map(replacer)));
 
-  return '<blockquote class="repost" data-domain="%domain%"><p>'+text+'</p></blockquote>';
+    return '<blockquote class="repost"><p>'+text+'</p></blockquote>';
+  });
+
 });
