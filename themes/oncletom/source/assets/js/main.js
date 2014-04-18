3058ca8087e779b62bb7152b5b726aeb1b9aca14
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-04-18 12:03:39 +0100

    Last tweaks and content sizing.

--- themes/oncletom/source/assets/js/main.js
+++ themes/oncletom/source/assets/js/main.js
@@ -50,30 +50,4 @@
 
       el.addEventListener('click', loadElement);
   });
-
-
-  var onScroll = (function(tag){
-    if (!document.querySelector){
-      return function(){};
-    }
-
-    var el = document.querySelector(tag);
-    var origOffsetY = el.offsetTop;
-
-    return function onScroll(e) {
-      window.scrollY >= origOffsetY ? el.classList.add('sticky') :
-        el.classList.remove('sticky');
-    };
-  })('aside');
-
-  var toggleCollapse = function(){
-    var target = document.querySelector(this.dataset.target || '.nav-collapse');
-
-    target.classList && target.classList.toggle('in');
-  };
-
-  if (document.querySelector && document.addEventListener){
-    document.addEventListener('scroll', onScroll);
-    document.querySelector('#services [data-toggle="collapse"]').addEventListener('click', toggleCollapse);
-  }
 })(document, window);
