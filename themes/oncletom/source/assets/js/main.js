01cdc4f268eb5ed09246f682a06945c0350b81ca
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-05-05 12:05:37 +0100

    Moving Typekit load even earlier in the network cascade.

--- themes/oncletom/source/assets/js/main.js
+++ themes/oncletom/source/assets/js/main.js
@@ -11,25 +11,8 @@
   context.disqus_shortname = tag.getAttribute("data-disqus-shortname");
   context.disqus_url = tag.getAttribute("href");
 
-  var loadScript = function(url, onReady){
-    var s = document.createElement('script');
-    s.src = url;
-
-    if (typeof onReady === 'function'){
-      s.onload = onReady;
-    }
-
-    (document.getElementsByTagName('body')[0]).appendChild(s);
-  };
-
-  loadScript('//use.typekit.net/ydq7bvz.js', function(){
-    try {
-      Typekit.load();
-    }
-    catch (e){}
-  });
-
   if (document.getElementById("disqus_thread")){
+    // loadScript is defined in the header of the page
     loadScript('//' + disqus_shortname + '.disqus.com/embed.js');
   }
 
