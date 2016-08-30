ed974e67f19cfadbccd5fea8b1c2add62b838cee
Thomas Parisot <thomas.parisot@bbc.co.uk>
2016-08-30 12:19:39 +0100

    Use hexo-bound `url_for` helper when NODE_ENV=production

--- scripts/cover-url.js
+++ scripts/cover-url.js
@@ -1,19 +1,22 @@
 'use strict';
 
-hexo.extend.helper.register('coverUrl', function(post, config){
+const urlFor = hexo.extend.helper.store['url_for'].bind(hexo);
+
+hexo.extend.helper.register('coverUrl', (post, config) => {
+  const { default_cover, url:siteUrl } = config;
   var url = post.cover ? (post.cover.url || post.cover) : '';
 
-  if (!url && config.default_cover) {
-    url = config.default_cover;
+  if (!url && default_cover) {
+    url = default_cover;
   }
 
   if (url){
     if (url.match(/^\/images/) && process.env.NODE_ENV === 'production'){
-      url = config.url + hexo.extend.helper.store['url_for'](url);
+      url = `${siteUrl}${urlFor(url)}`;
     }
 
     if (url.match(/^\/\//)){
-      url = 'https:' + url;
+      url = `https:${url}`;
     }
 
     url = url.replace('_c_d.jpg', '_b_d.jpg');
