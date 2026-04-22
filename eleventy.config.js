28f0ace8cdfaccd562e84fc68cce9d14bafb0b5f
Thomas Parisot <thom4parisot@users.noreply.github.com>
2026-04-22 19:39:17 +0200

    feat(a11y): make all emojis and external links accessible

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,7 +7,10 @@ import mime from 'mime/lite'
 import mdAnchor from 'markdown-it-anchor'
 import mdAttrs from 'markdown-it-attrs'
 import mdContainer from 'markdown-it-container'
+import { full as mdEmoji } from 'markdown-it-emoji'
+import emojiPairs from 'markdown-it-emoji/lib/data/full.mjs'
 import mdFootnote from 'markdown-it-footnote'
+import mdIterator from 'markdown-it-for-inline'
 import mdMark from 'markdown-it-mark'
 import mdSpans from 'markdown-it-bracketed-spans'
 import toml from '@iarna/toml'
@@ -68,6 +71,11 @@ export default async function(eleventyConfig) {
 	})
 
   eleventyConfig.amendLibrary('md', (mdLib) => {
+    const emoToText = Object.fromEntries(
+      Object.entries(emojiPairs).map(([key, value]) => ([value, key]))
+    )
+    const emoRe = new RegExp(`(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])`, 'g')
+
     const lib = mdLib
       .use(mdSpans)
       .use(mdAttrs)
@@ -100,6 +108,48 @@ export default async function(eleventyConfig) {
           }
         }
       })
+      .use(mdIterator, 'external_link', 'link_open', function (tokens, idx) {
+        const href = tokens[idx].attrGet('href')
+
+        if (href && (href.startsWith('http://') || href.startsWith('https://'))) {
+          tokens[idx].attrJoin('class', 'external-link')
+          tokens[idx].attrJoin('rel', 'noopener')
+          tokens[idx].attrJoin('rel', 'noreferrer')
+          tokens[idx].attrSet('target', '_blank')
+        }
+      })
+      .use(mdEmoji)
+
+      lib.core.ruler.before('emoji', 'emoji_to_text', function replace (state) {
+        let token
+        const blockTokens = state.tokens
+
+        for (let j = 0, l = blockTokens.length; j < l; j++) {
+          if (blockTokens[j].type !== 'inline') { continue }
+          let tokens = blockTokens[j].children
+
+          // We scan from the end, to keep position when new tags added.
+          // Use reversed logic in links start/end match
+          for (let idx = tokens.length - 1; idx >= 0; idx--) {
+            token = tokens[idx]
+
+            if (token.type === 'text' && emoRe.test(token.content)) {
+
+              tokens[idx].content = tokens[idx].content.replace(emoRe, function (match, emoji, src) {
+                if (emoToText[emoji]) {
+                  return `:${emoToText[emoji]}:`
+                }
+
+                return emoji
+              })
+            }
+          }
+        }
+      })
+
+      lib.renderer.rules.emoji = function renderEmoji (token, idx) {
+        return `<span class="emoji emoji--${token[idx].markup}" aria-hidden="true">${token[idx].content}</span>`
+      }
 
       eleventyConfig.addBundle("html", {
         transforms: [
